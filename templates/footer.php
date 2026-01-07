e4ccb9cc1392e018de624b928607f31f6378919e
CUTILLAS LÓPEZ <alvcutlop@alu.edu.gva.es>
2026-01-07 14:28:41 +0100

    Commit 1 de Alvaro

---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+    <footer>
+        <h4>Contacto</h4>
+        <p>666 777 888 - 888 777 666</p>
+        <p>correoempresa@gmail.com</p>
+        <p id="fecha"><?php
+            echo date('d/m/Y');
+        ?></p>
+    </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control de EPI</title>
+    <link rel="stylesheet" href="static/estilos/style.css">
+</head>
+<body>
+    <header>
+        <h1>Sistema de gestión de la seguridad laboral</h1><img src="./static/includes/logo.png" height="60px">
+    </header>
