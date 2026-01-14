7bc5f344c562241cf587defb0c5c6f9f0fd28cb3
MARTINEZ FULGENCIO <jonmarful@alu.edu.gva.es>
2026-01-14 14:16:24 +0100

    Commit de prueba de la rama jonhy

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
     <footer>
         <h4>Contacto</h4>
-        <p>666 777 888 - 888 777 666</p>
+        <p>666 777 888 - 645 777 666</p>
         <p>correoempresa@gmail.com</p>
         <p id="fecha"><?php
             echo date('d/m/Y');
